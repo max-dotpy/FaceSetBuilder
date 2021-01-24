6616306f0fbc989d529e48b495401c3222d745b2
max <massimo.cosimo@studenti.unipd.it>
2021-01-24 11:23:53 +0100

    Initial commit

---
+++ FaceSetMaker/__init__.py
@@ -0,0 +1,16 @@
+from FaceSetMaker.board import Board
+from FaceSetMaker.manager import Manager
+from FaceSetMaker.myTkinter import myButton
+from FaceSetMaker.palette import Palette
+from FaceSetMaker.selector import Selector
+from FaceSetMaker.settings import *
+from FaceSetMaker.writer import Writer
+from FaceSetMaker.app import *
+
+
+if __name__ == "__main__":
+    root = Tk()
+
+    App(root)
+
+    root.mainloop()
